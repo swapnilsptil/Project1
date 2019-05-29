2f044610ae05e61197c1d7ef4941a7ba7d70cbe5
swapnil <swapnils.ptil@gmail.com>
2019-05-29 21:14:49 +0530

    MongoDB Pointing to Atlas

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,10 +1,10 @@
 const config = require('config.json');
 const mongoose = require('mongoose');
 //Atlas
-// mongoose.connect(process.env.MONGODB_URI || config.connectionStringAtlas, { useCreateIndex: true, useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || config.connectionStringAtlas, { useCreateIndex: true, useNewUrlParser: true });
 
 //Local Mongodb
-mongoose.connect(process.env.MONGODB_URI || config.connectionStringLocal, { useCreateIndex: true, useNewUrlParser: true });
+// mongoose.connect(process.env.MONGODB_URI || config.connectionStringLocal, { useCreateIndex: true, useNewUrlParser: true });
 
 mongoose.Promise = global.Promise;
 
