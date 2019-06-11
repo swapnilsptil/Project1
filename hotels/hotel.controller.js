06e7bb9e683df10904e266b10a032f4479e3131e
swapnil <swapnils.ptil@gmail.com>
2019-06-11 17:10:27 +0530

    Adding fetch Hotels by menu

--- hotels/hotel.controller.js
+++ hotels/hotel.controller.js
@@ -7,6 +7,8 @@ const hotelService = require('./hotel.service');
 // router.post('/authenticate', authenticate);
 router.post('/register', register);
 router.get('/', getAll);
+router.post('/updateMenu', updateMenu);
+router.post('/findHotelByMenu/:id', findHotelByMenu);
 // router.get('/current', getCurrent);
 // router.get('/:id', getById);
 // router.put('/:id', update);
@@ -32,11 +34,17 @@ function getAll(req, res, next) {
         .catch(err => next(err));
 }
 
-// function getCurrent(req, res, next) {
-//     userService.getById(req.user.sub)
-//         .then(user => user ? res.json(user) : res.sendStatus(404))
-//         .catch(err => next(err));
-// }
+function updateMenu(req, res, next) {
+    hotelService.updateMenu(req.params.id, req.body)
+        .then(user => user ? res.json(user) : res.sendStatus(404))
+        .catch(err => next(err));
+}
+
+function findHotelByMenu(req, res, next) {
+    hotelService.findHotelByMenu(req.body)
+        .then(hotel => {hotel ? res.json(hotel) : res.sendStatus(404)})
+        .catch(err => next(err));
+}
 
 // function getById(req, res, next) {
 //     userService.getById(req.params.id)
--- hotels/hotel.service.js
+++ hotels/hotel.service.js
@@ -7,7 +7,9 @@ const Hotel = db.Hotel;
 
 module.exports = {
     getAll,
-    create
+    create,
+    updateMenu,
+    findHotelByMenu
 }
 
 async function create(userParam) {
@@ -21,57 +23,43 @@ async function create(userParam) {
         hotel.hash = bcrypt.hashSync(userParam.hotelName, 10);
     }
 
-    // // hash password
-    // if (userParam.password) {
-    //     user.hash = bcrypt.hashSync(userParam.password, 10);
-    // }
-
-    // save user
     await hotel.save();
 }
-
-// async function authenticate({ username, password }) {
-//     const user = await User.findOne({ username });
-//     if (user && bcrypt.compareSync(password, user.hash)) {
-//         const { hash, ...userWithoutHash } = user.toObject();
-//         const token = jwt.sign({ sub: user.id }, config.secret);
-//         // console.log('user Details', ...userWithoutHash);
-//         return {
-//             ...userWithoutHash,
-//             token
-//         };
-//     }
-// }
 
 async function getAll() {
     return await Hotel.find();
 }
 
-// async function getById(id) {
-//     return await User.findById(id);
-// }
 
+async function updateMenu(id, userParam) {
+    const hotel = await Hotel.findById(id);
 
-// async function update(id, userParam) {
-//     const user = await User.findById(id);
+    // validate
+    if (!hotel) throw 'Hotel not found';
+    if (hotel.username !== userParam.username && await Hotel.findOne({ username: userParam.username })) {
+        throw 'Username "' + hotel.username + '" is already taken';
+    }
 
-//     // validate
-//     if (!user) throw 'User not found';
-//     if (user.username !== userParam.username && await User.findOne({ username: userParam.username })) {
-//         throw 'Username "' + userParam.username + '" is already taken';
-//     }
+    // hash password if it was entered
+    if (userParam.password) {
+        userParam.hash = bcrypt.hashSync(userParam.password, 10);
+    }
 
-//     // hash password if it was entered
-//     if (userParam.password) {
-//         userParam.hash = bcrypt.hashSync(userParam.password, 10);
-//     }
+    // copy userParam properties to user
+    Object.assign(hotel, userParam);
 
-//     // copy userParam properties to user
-//     Object.assign(user, userParam);
+    await hotel.save();
+}
 
-//     await user.save();
-// }
+async function findHotelByMenu(req) {
+    // const hotel = await Hotel.find( { ratings: { $all: [ "value", "count", "book" ] } } );
+    // return await Hotel.find({'menu.' : 4});
+    return await Hotel.find({menu: {$elemMatch: {mealType : req.mealType}}});
+    
+    // console.log(hotel);
+}
 
-// async function _delete(id) {
-//     await User.findByIdAndRemove(id);
-// }
+async function _delete(id) {
+    await User.findByIdAndRemove(id);
+}
+
